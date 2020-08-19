d0bb89643425870b72a1db347d488d9b891e9dbf
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2020-08-19 16:17:35 -0400

    Fix compiling warning

--- js/src/components/Updater.tsx
+++ js/src/components/Updater.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { EuiConfirmModal, EuiOverlayMask, EuiText, } from '@elastic/eui';
 import { ItemDstLink } from './Snippets';
